b162b8c25ddccf910d33e67c4f4294b57de64462
David Freeman <davidfreeman@Davids-MacBook-Pro.local>
2017-04-14 20:42:33 -0500

    added attempt loop

--- genetic.py
+++ genetic.py
@@ -4,11 +4,11 @@ import console
 import time
 
 cross = 0.7
-mutation = 0.01
+mutation = 0.002
 pop_size = 100
 chromo_length = 104
 gene_length = 4
-max_gens = 3000
+max_gens = 5000
 gens = 0
 
 def fitness(population):
@@ -22,7 +22,7 @@ def fitness(population):
                 console.printBoard()
         board.setBoard()
         console.println("Generation: " + str(gens) + " Genome: " + str(i) + " Fitness: " + str(population[i]['fitness']))
-        #time.sleep(1)
+        #time.sleep(0.5)
 
 def chooseParent(population):
     maximum = sum([child['fitness'] for child in population])
@@ -63,32 +63,43 @@ def randomBits(length):
             bits += '0'
     return bits
 
-population = [None] * pop_size
-for i in range(0, pop_size):
-    population[i] = {'dna': randomBits(chromo_length), 'fitness': 0}
-gens = 0
-found = False
-while not found:
-    fitness(population)
-    for child in population:
-        if child['fitness'] == 13:
-            found = True
+attempt = 0
+while True:
+    print("Attempt #" + str(attempt))
+
+    population = [None] * pop_size
+    for i in range(0, pop_size):
+        population[i] = {'dna': randomBits(chromo_length), 'fitness': 0}
+    gens = 0
+    found = False
+
+    while not found:
+        fitness(population)
+        for child in population:
+            if child['fitness'] == 13:
+                console.println("Found solution: " + child['dna'])
+                found = True
+                break
+
+        temp = [None] * pop_size
+        for i in range(0, pop_size, 2):
+            offspring1 = chooseParent(population)
+            offspring2 = chooseParent(population)
+            crsovr = crossover(offspring1, offspring2)
+            offspring1 = crsovr[0]
+            offspring2 = crsovr[1]
+            offspring1 = mutate(offspring1)
+            offspring2 = mutate(offspring2)
+            temp[i] = {'dna': offspring1, 'fitness': 0}
+            temp[i+1] = {'dna': offspring2, 'fitness': 0}
+        for i in range(0, pop_size):
+            population[i] = temp[i]
+
+        gens += 1
+        if gens > max_gens:
+            console.println("No solution found this run")
             break
 
-    temp = [None] * pop_size
-    for i in range(0, pop_size, 2):
-        offspring1 = chooseParent(population)
-        offspring2 = chooseParent(population)
-        crsovr = crossover(offspring1, offspring2)
-        offspring1 = crsovr[0]
-        offspring2 = crsovr[1]
-        offspring1 = mutate(offspring1)
-        offspring2 = mutate(offspring2)
-        temp[i] = {'dna': offspring1, 'fitness': 0}
-        temp[i+1] = {'dna': offspring2, 'fitness': 0}
-    for i in range(0, len(temp)):
-        population[i] = temp[i]
-    gens += 1
-    if gens > max_gens:
-        console.println("No solution found this run")
-        found = True
+    if found:
+        break
+    attempt += 1
